207fc101bfb1145b0fe9c474f9413e513d423a06
pagial <Pagial.Menyatsoe@absa.africa>
2021-12-24 10:22:42 +0200

    init

---
+++ src/extensions/googleAnalytics2019/loc/en-us.js
@@ -0,0 +1,5 @@
+define([], function() {
+  return {
+    "Title": "GoogleAnalytics2019ApplicationCustomizer"
+  }
+});
---
+++ src/extensions/googleAnalytics2019/loc/myStrings.d.ts
@@ -0,0 +1,9 @@
+declare interface IGoogleAnalytics2019ApplicationCustomizerStrings {
+  Title: string;
+  MissingID: "Tracking ID missing";
+}
+
+declare module "GoogleAnalytics2019ApplicationCustomizerStrings" {
+  const strings: IGoogleAnalytics2019ApplicationCustomizerStrings;
+  export = strings;
+}
